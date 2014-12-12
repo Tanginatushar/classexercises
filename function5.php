fdc27c7a19903a892b59a4e7c66c41bdda5f6cf5
tanjina <tanjina_tushar@yahoo.com>
2014-12-12 10:32:15 +0600

    Incorrect usage of default function arguments

---
+++ function5.php
@@ -0,0 +1,10 @@
+
+<?php
+function makeyogurt($type = "acidophilus", $flavour)
+{
+    return "Making a bowl of $type $flavour.\n";
+}
+
+echo makeyogurt("raspberry");   // won't work as expected
+?>
+
